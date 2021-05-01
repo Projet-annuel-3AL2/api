d4c6afe3dd6972c100fc90d3f6422d72a2115088
pooetitu <pooetitu@gmail.com>
2021-05-01 02:59:00 +0200

    Add project sub route

--- src/organization-app/routes/index.route.ts
+++ src/organization-app/routes/index.route.ts
@@ -4,6 +4,7 @@ import {configure} from "../config/passport.config";
 import {authRouter} from "./auth.route";
 import passport from "passport";
 import {ensureLoggedIn} from "../middlewares/auth.middleware";
+import {projectRouter} from "./project.route";
 
 export function buildOrgAppRoutes() {
     const router = Router();
@@ -11,6 +12,7 @@ export function buildOrgAppRoutes() {
     router.use(passport.initialize());
     router.use(passport.session());
     router.use("/user", ensureLoggedIn, userRouter);
+    router.use("/project", ensureLoggedIn, projectRouter);
     router.use("/auth", authRouter);
     return router;
 }
