a92340e391fddd3b0812ebf63e96a0f739a46f4c
pooetitu <pooetitu@gmail.com>
2021-05-23 01:02:19 +0200

    Add post creation for user

--- src/agir-pour-tous/controllers/post.controller.ts
+++ src/agir-pour-tous/controllers/post.controller.ts
@@ -1,14 +1,16 @@
 import {getRepository, Repository} from "typeorm";
 import {Post, PostProps} from "../models/post.model";
+import {User} from "../models/user.model";
+import {validate} from "class-validator";
 
 export class PostController {
 
     private static instance: PostController;
 
-    private PostRepository: Repository<Post>;
+    private postRepository: Repository<Post>;
 
     private constructor() {
-        this.PostRepository = getRepository(Post);
+        this.postRepository = getRepository(Post);
     }
 
     public static getInstance(): PostController {
@@ -18,20 +20,29 @@ export class PostController {
         return PostController.instance;
     }
 
+    public async create(user: User, props: PostProps): Promise<Post> {
+        const post = this.postRepository.create({...props, creator: user});
+        const err = await validate(post);
+        if(err.length > 0) {
+            throw err;
+        }
+        return this.postRepository.save(post);
+    }
+
     public async getById(postId: string): Promise<Post> {
-        return await this.PostRepository.findOneOrFail(postId);
+        return await this.postRepository.findOneOrFail(postId);
     }
 
     public async getAll(): Promise<Post[]> {
-        return await this.PostRepository.find();
+        return await this.postRepository.find();
     }
 
     public async delete(postId: string): Promise<void> {
-        await this.PostRepository.softDelete(postId);
+        await this.postRepository.softDelete(postId);
     }
 
     public async update(postId: string, props: PostProps): Promise<Post> {
-        await this.PostRepository.update(postId, props);
+        await this.postRepository.update(postId, props);
         return this.getById(postId);
     }
 
--- src/agir-pour-tous/routes/post.route.ts
+++ src/agir-pour-tous/routes/post.route.ts
@@ -1,8 +1,19 @@
 import express from "express";
 import {ensureLoggedIn} from "../middlewares/auth.middleware";
 import {PostController} from "../controllers/post.controller";
+import {User} from "../models/user.model";
 
 const postRouter = express.Router();
+
+postRouter.post('/', ensureLoggedIn, async (req, res) => {
+    try {
+        const postController = PostController.getInstance();
+        const post = postController.create(req.user as User, req.body);
+        res.json(post);
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
 
 postRouter.get('/', async (req, res) => {
     try {
