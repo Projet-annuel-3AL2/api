e11f6b0b36b87adae9ca50f47d55a981c4445d68
pooetitu <pooetitu@gmail.com>
2021-05-22 22:42:43 +0200

    Add super admin rights to admin seeded user

--- src/agir-pour-tous/database/seeds/user.seed.ts
+++ src/agir-pour-tous/database/seeds/user.seed.ts
@@ -1,13 +1,14 @@
 import {Factory, Seeder} from "typeorm-seeding";
 import {Connection} from "typeorm";
-import {User} from "../../models/user.model";
+import {User, UserType} from "../../models/user.model";
 
 export class CreateUser implements Seeder {
     public async run(factory: Factory, connection: Connection): Promise<any> {
         await factory(User)({
             username: process.env.SUPERADMIN_USERNAME,
             email: process.env.SUPERADMIN_EMAIL,
-            password: process.env.SUPERADMIN_PASSWD
+            password: process.env.SUPERADMIN_PASSWD,
+            userType: UserType.SUPER_ADMIN
         });
         await factory(User)({}).createMany(10);
     }
