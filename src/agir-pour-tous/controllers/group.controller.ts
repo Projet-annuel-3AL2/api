f2f2d077030a1391f76e0bbd000b41968ee94765
pooetitu <pooetitu@gmail.com>
2021-05-23 16:19:20 +0200

    Add post creation on group

--- src/agir-pour-tous/controllers/group.controller.ts
+++ src/agir-pour-tous/controllers/group.controller.ts
@@ -1,5 +1,5 @@
 import {getRepository, Repository} from "typeorm";
-import {Post} from "../models/post.model";
+import {Post, PostProps} from "../models/post.model";
 import {Group, GroupProps} from "../models/group.model";
 import {User} from "../models/user.model";
 import {validate} from "class-validator";
@@ -55,5 +55,14 @@ export class GroupController {
             .leftJoin("Post.group", "Group")
             .where("Group.name=:groupName", {groupName})
             .getMany();
+    }
+
+    public async addPost(group: Group, creator: User, props: PostProps): Promise<Post> {
+        const post = getRepository(Post).create({...props, creator, group});
+        const err = await validate(post);
+        if(err.length > 0){
+            throw err;
+        }
+        return getRepository(Post).save(post);
     }
 }
