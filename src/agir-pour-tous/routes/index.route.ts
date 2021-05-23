010618a6d4846c1ec80fa48353c1a52716ef16be
pooetitu <pooetitu@gmail.com>
2021-05-24 01:34:28 +0200

    Fix APT session

--- src/agir-pour-tous/routes/index.route.ts
+++ src/agir-pour-tous/routes/index.route.ts
@@ -4,12 +4,18 @@ import {TypeormStore} from "connect-typeorm";
 import {getRepository} from "typeorm";
 import {Session} from "../models/session.model";
 import {configure} from "../config/passport.config";
+import {authRouter} from "./auth.route";
+import {userRouter} from "./user.route";
+import {categoryRouter} from "./category.route";
+import {groupRouter} from "./group.route";
+import {organisationRouter} from "./organisation.route";
+import {friendshipRouter} from "./friendship.route";
+import {postRouter} from "./post.route";
+import {conversationRouter} from "./conversation.route";
 
 export function buildAPTRoutes() {
     const router = Router();
     configure();
-    router.use(passport.initialize());
-    router.use(passport.session());
     router.use(require('express-session')({
         secret: process.env.ORG_APP_SECRET,
         resave: true,
@@ -20,6 +26,15 @@ export function buildAPTRoutes() {
             ttl: 259200
         }).connect(getRepository(Session)),
     }));
-
+    router.use(passport.initialize());
+    router.use(passport.session());
+    router.use("/auth", authRouter);
+    router.use("/user", userRouter);
+    router.use("/category", categoryRouter);
+    router.use("/group", groupRouter);
+    router.use("/organisation", organisationRouter);
+    router.use("/post", postRouter);
+    router.use("/friendship", friendshipRouter);
+    router.use("/conversation", conversationRouter);
     return router;
 }
