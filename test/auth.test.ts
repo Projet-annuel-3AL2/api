677f662e6c94e65c2b5fb2791a8b6fc1258b0a29
pooetitu <pooetitu@gmail.com>
2021-05-24 02:10:28 +0200

    Add logout and register tests

--- test/auth.test.ts
+++ test/auth.test.ts
@@ -2,14 +2,14 @@ import express from "express";
 import {buildAPTRoutes} from "../src/agir-pour-tous/routes/index.route";
 import {createConnection} from "typeorm";
 import {config} from "dotenv";
-import {factory, tearDownDatabase, useSeeding} from "typeorm-seeding";
+import {factory, tearDownDatabase, useRefreshDatabase, useSeeding} from "typeorm-seeding";
 import {User} from "../src/agir-pour-tous/models/user.model";
 import bodyParser from "body-parser";
 import session from "supertest-session";
+import * as faker from 'faker';
 
 config();
 const app = express();
-let testSession;
 describe("Auth", () => {
     beforeAll(async done=>{
         await createConnection();
@@ -18,11 +18,14 @@ describe("Auth", () => {
         app.use(bodyParser.urlencoded({extended: false}));
         app.use(require('cookie-parser')());
         app.use("/", buildAPTRoutes());
+        await useRefreshDatabase()
         done();
     });
     describe("Login", ()=>{
+        let testSession;
+        let user: User;
         beforeAll(async done=>{
-            await factory(User)({"username":"admin", "password":"admin"}).create();
+            user = await factory(User)({"username":"admin", "password":"admin"}).create();
             testSession = session(app);
             done();
         });
@@ -31,7 +34,7 @@ describe("Auth", () => {
                 .post("/auth/login")
                 .send({username:"admin", password:"admin"});
             expect(status).toEqual(200);
-            expect(body.username).toEqual("admin");
+            expect(body.username).toEqual(user.username);
         });
         it("Should refuse to login the user if he's already logged on this session", async ()=> {
             const { body, status } = await testSession
@@ -39,6 +42,66 @@ describe("Auth", () => {
                 .send({username:"admin", password:"admin"});
             expect(status).toEqual(401);
             expect(body).toEqual({});
+        });
+    });
+    describe("Logout", ()=>{
+        let testSession;
+        testSession = session(app);
+        beforeAll(async done=>{
+            await testSession.post("/auth/login")
+                .send({username:"admin", password:"admin"});
+            done();
+        });
+        it("Should disconnect the user if he is logged on the session", async ()=>{
+            const {status}=await testSession.delete("/auth/logout");
+            expect(status).toEqual(204);
+        });
+        it("Should refuse the user to log out if he is not logged on the session", async ()=>{
+            const {status}=await testSession.delete("/auth/logout");
+            expect(status).toEqual(401);
+        });
+    });
+    describe("Register", ()=>{
+        let testSession;
+        let user;
+        beforeAll(async done=>{
+            user = {
+                username: faker.random.alphaNumeric(20),
+                mail: faker.internet.email(),
+                password: faker.random.alphaNumeric(30)
+            }
+            testSession = session(app);
+            done();
+        });
+        it("Should register the new user", async () => {
+            const { body, status } = await testSession
+                .post("/auth/register")
+                .send(user);
+            expect(status).toEqual(200);
+            expect(body.username).toEqual(user.username);
+            expect(body.mail).toEqual(user.mail);
+        });
+        it("Should refuse to register if the mail is already taken", async ()=> {
+            const newUser = {
+                username: faker.random.alphaNumeric(20),
+                mail: user.mail,
+                password: faker.random.alphaNumeric(30)
+            }
+            const {  status } = await testSession
+                .post("/auth/register")
+                .send(newUser);
+            expect(status).toEqual(400);
+        });
+        it("Should refuse to register if the username is already taken", async ()=> {
+            const newUser = {
+                username: user.username,
+                mail: faker.internet.email(),
+                password: faker.random.alphaNumeric(30)
+            }
+            const {  status } = await testSession
+                .post("/auth/register")
+                .send(newUser);
+            expect(status).toEqual(400);
         });
     });
     afterAll(async done => {
