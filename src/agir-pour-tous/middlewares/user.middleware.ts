5a64152e21a62f309e36df68552d4a49472625ec
pooetitu <pooetitu@gmail.com>
2021-06-17 20:48:03 +0200

    Fix is asked user middleware

--- src/agir-pour-tous/middlewares/user.middleware.ts
+++ src/agir-pour-tous/middlewares/user.middleware.ts
@@ -1,7 +1,7 @@
 import {User} from "../models/user.model";
 
 export async function isAskedUser(req, res, next) {
-    if (req.user && (req.user as User).username == req.params.username) {
+    if (req.user && (req.user as User).id === req.params.userId) {
         next();
     }
     res.status(404).end();
