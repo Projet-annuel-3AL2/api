e648526000215dba548d04712844b78bacedd242
pooetitu <pooetitu@gmail.com>
2021-06-17 20:52:43 +0200

    Fix is asked user always returning 404

--- src/agir-pour-tous/middlewares/user.middleware.ts
+++ src/agir-pour-tous/middlewares/user.middleware.ts
@@ -1,8 +1,8 @@
 import {User} from "../models/user.model";
 
 export async function isAskedUser(req, res, next) {
-    if (req.user && (req.user as User).id === req.params.userId) {
-        next();
+    if (!(req.user && (req.user as User).id === req.params.userId)) {
+        return res.status(404).end();
     }
-    res.status(404).end();
+    next();
 }
