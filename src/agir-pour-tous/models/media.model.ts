5da6d4e53b1a3b177d5c591d47d9c0609e320a91
pooetitu <pooetitu@gmail.com>
2021-07-25 15:47:51 +0200

    Fix cascade

--- src/agir-pour-tous/models/media.model.ts
+++ src/agir-pour-tous/models/media.model.ts
@@ -22,19 +22,19 @@ export class Media {
     id: string;
     @Column({nullable: false})
     link: string;
-    @OneToOne(() => User, user => user.profilePicture,{cascade:true, onDelete:'CASCADE'})
+    @OneToOne(() => User, user => user.profilePicture,{onDelete:'CASCADE'})
     userProfilePicture: User;
-    @OneToOne(() => User, user => user.bannerPicture,{cascade:true, onDelete:'CASCADE'})
+    @OneToOne(() => User, user => user.bannerPicture,{ onDelete:'CASCADE'})
     userBanner: User;
-    @ManyToOne(() => Post, post => post.medias,{cascade:true, onDelete:'CASCADE'})
+    @ManyToOne(() => Post, post => post.medias,{onDelete:'CASCADE'})
     post: Post;
-    @OneToOne(() => Organisation, organisation => organisation.profilePicture,{cascade:true, onDelete:'CASCADE'})
+    @OneToOne(() => Organisation, organisation => organisation.profilePicture,{onDelete:'CASCADE'})
     organisationProfilePicture: Organisation;
-    @OneToOne(() => Organisation, organisation => organisation.bannerPicture,{cascade:true, onDelete:'CASCADE'})
+    @OneToOne(() => Organisation, organisation => organisation.bannerPicture,{onDelete:'CASCADE'})
     organisationBannerPicture: Organisation;
-    @ManyToOne(() => Comment, comment => comment.medias,{cascade:true, onDelete:'CASCADE'})
+    @ManyToOne(() => Comment, comment => comment.medias,{onDelete:'CASCADE'})
     comments: Comment;
-    @OneToOne(() => Event, event => event.picture,{cascade:true, onDelete:'CASCADE'})
+    @OneToOne(() => Event, event => event.picture,{onDelete:'CASCADE'})
     eventPicture: Event;
     @CreateDateColumn()
     createdAt: Date;
